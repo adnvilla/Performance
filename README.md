d021d16bb3c723df307f31855820811be44647d4
AdnVilla <adnvilla@gmail.com>
2018-03-17 22:55:27 -0500

    Add Exceptions bench

--- README.md
+++ README.md
@@ -23,3 +23,38 @@ ClassTupleAsKey | 7,001.1404 ms | 138.3584 ms | 1,071.7192 ms | 6,607.0421 ms | 
 StructTupleAsKey | 7,046.6457 ms |  28.2645 ms |   218.9356 ms | 6,958.6388 ms | 638950.0000 | 135050.0000 | 6000.0000 |   1.65 GB |
 MyStructAsKey | 2,399.8246 ms |  12.4823 ms |    96.6873 ms | 2,388.9916 ms | 127100.0000 |  49550.0000 | 4050.0000 |    765 MB | 
 MyStructAsKeyWithCompare |   955.2248 ms |   5.2553 ms |    40.7071 ms |   938.4278 ms |  11950.0000 |   8900.0000 | 2950.0000 |   59.6 MB |
+
+
+
+
+
+
+
+# Exceptions Bench
+
+``` ini
+
+BenchmarkDotNet=v0.10.3.0, OS=Microsoft Windows NT 6.2.9200.0
+Processor=Intel(R) Core(TM) i7-5500U CPU 2.40GHz, ProcessorCount=4
+Frequency=2338337 Hz, Resolution=427.6544 ns, Timer=TSC
+  [Host]     : Clr 4.0.30319.42000, 32bit LegacyJIT-v4.7.2633.0
+  DefaultJob : Clr 4.0.30319.42000, 32bit LegacyJIT-v4.7.2633.0
+
+
+```
+ |                        Method |            Mean |        StdDev |    Scaled | Scaled-StdDev |
+ |------------------------------ |---------------- |-------------- |---------- |-------------- |
+ |      ErrorCodeWithReturnValue |      12.2150 ns |     0.1463 ns |      1.00 |          0.00 |
+ |       RareExceptionStackTrace |      26.4149 ns |     0.2138 ns |      2.16 |          0.03 |
+ | RareExceptionMediumStackTrace |      70.1164 ns |     0.4487 ns |      5.74 |          0.07 |
+ |   RareExceptionDeepStackTrace |     108.7963 ns |     0.9961 ns |      8.91 |          0.13 |
+ |             ExceptionTryCatch |  13,891.9944 ns |   149.2097 ns |  1,137.44 |         17.51 |
+ |              ExceptionMessage |  13,949.2975 ns |    70.2262 ns |  1,142.13 |         14.14 |
+ |        ExceptionMediumMessage |  20,045.6745 ns |   155.5210 ns |  1,641.28 |         22.37 |
+ |       ExceptionMediumTryCatch |  20,385.6284 ns |   186.9361 ns |  1,669.12 |         24.09 |
+ |          ExceptionDeepMessage |  25,456.5397 ns |   231.1298 ns |  2,084.31 |         29.94 |
+ |         ExceptionDeepTryCatch |  25,535.8895 ns |   127.3833 ns |  2,090.80 |         25.86 |
+ |     CachedExceptionStackTrace |  36,777.4288 ns |   187.0627 ns |  3,011.23 |         37.36 |
+ |           ExceptionStackTrace |  45,445.3053 ns |   315.2136 ns |  3,720.93 |         49.18 |
+ |     ExceptionMediumStackTrace | 116,205.4792 ns |   639.9017 ns |  9,514.56 |        119.63 |
+ |       ExceptionDeepStackTrace | 175,454.3172 ns | 1,559.0901 ns | 14,365.68 |        204.93 |
---
+++ src/Exceptions/Program.cs
@@ -0,0 +1,499 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Exceptions
+{
+    [Config(typeof(Config))]
+    [OrderProvider(SummaryOrderPolicy.FastestToSlowest)]
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Program>();
+        }
+
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(RPlotExporter.Default);
+            }
+        }
+
+        public struct ResultAndErrorCode<T>
+        {
+            public T Result;
+            public int ErrorCode;
+        }
+
+        [Benchmark(Baseline = true)]
+        public ResultAndErrorCode<string> ErrorCodeWithReturnValue()
+        {
+            var result = new ResultAndErrorCode<string>();
+            result.Result = null;
+            result.ErrorCode = 5;
+            return result;
+        }
+
+        [Benchmark]
+        public Exception ExceptionTryCatch()
+        {
+            try
+            {
+                Level20(); // start *all* the way down the stack
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                return ioex;
+            }
+        }
+
+        [Benchmark]
+        public Exception ExceptionMediumTryCatch()
+        {
+            try
+            {
+                Level10(); // start 1/2 way down
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex;
+            }
+        }
+
+        [Benchmark]
+        public Exception ExceptionDeepTryCatch()
+        {
+            try
+            {
+                Level1();
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex;
+            }
+        }
+
+        [Benchmark]
+        public string ExceptionMessage()
+        {
+            try
+            {
+                Level20(); // start *all* the way down the stack
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Only get the simple message from the Exception (don't trigger a StackTrace collection)
+                return ioex.Message;
+            }
+        }
+
+        [Benchmark]
+        public string ExceptionMediumMessage()
+        {
+            try
+            {
+                Level10(); // start 1/2 way down
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Only get the simple message from the Exception (don't trigger a StackTrace collection)
+                return ioex.Message;
+            }
+        }
+
+        [Benchmark]
+        public string ExceptionDeepMessage()
+        {
+            try
+            {
+                Level1();
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Only get the simple message from the Exception (don't trigger a StackTrace collection)
+                return ioex.Message;
+            }
+        }
+
+        [Benchmark]
+        public string ExceptionStackTrace()
+        {
+            try
+            {
+                Level20(); // start *all* the way down the stack
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex.StackTrace;
+            }
+        }
+
+        private static readonly InvalidOperationException cachedEx = new InvalidOperationException("Benchmark");
+
+        [Benchmark]
+        public string CachedExceptionStackTrace()
+        {
+            try
+            {
+                throw cachedEx;
+            }
+            catch (InvalidOperationException ioEx)
+            {
+                return ioEx.StackTrace;
+            }
+        }
+
+        [Benchmark]
+        public string RareExceptionStackTrace()
+        {
+            try
+            {
+                RareLevel20(); // start all the way down
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex.StackTrace;
+            }
+        }
+
+        [Benchmark]
+        public string ExceptionMediumStackTrace()
+        {
+            try
+            {
+                Level10(); // start 1/2 way down
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex.StackTrace;
+            }
+        }
+
+        [Benchmark]
+        public string RareExceptionMediumStackTrace()
+        {
+            try
+            {
+                RareLevel10(); // start 1/2 way down
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex.StackTrace;
+            }
+        }
+
+        [Benchmark]
+        public string ExceptionDeepStackTrace()
+        {
+            try
+            {
+                Level1();
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex.StackTrace;
+            }
+        }
+
+        [Benchmark]
+        public string RareExceptionDeepStackTrace()
+        {
+            try
+            {
+                RareLevel1();
+                return null; //Prevent Error CS0161: not all code paths return a value
+            }
+            catch (InvalidOperationException ioex)
+            {
+                // Force collection of a full StackTrace
+                return ioex.StackTrace;
+            }
+        }
+
+        // Impact Probability (cumulative) 3.7e-04 = 0.00037 = 0.037000000% chance of Earth impact
+        // or 1 in 2,700 chance or 99.96300000% chance the asteroid will miss the Earth
+        // See http://www.universetoday.com/69640/researchers-say-asteroid-has-1-in-1000-chance-of-hitting-earth-in-2182/
+        // and http://neo.jpl.nasa.gov/risk/a101955.html#legend and http://neo.jpl.nasa.gov/cgi-bin/ip?3.7e-04
+        /// <summary> 1 in 2,700 chance - Impact Probability (cumulative) = 0.037000000% (99.96300000% chance the asteroid will miss the Earth)</summary>
+        private static long chanceOfAsteroidHit = 2700;
+
+        private static long counter;
+
+        [Setup]
+        public void Setup()
+        {
+            counter = 0;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level1()
+        {
+            Level2();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level2()
+        {
+            Level3();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level3()
+        {
+            Level4();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level4()
+        {
+            Level5();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level5()
+        {
+            Level6();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level6()
+        {
+            Level7();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level7()
+        {
+            Level8();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level8()
+        {
+            Level9();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level9()
+        {
+            Level10();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level10()
+        {
+            Level11();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level11()
+        {
+            Level12();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level12()
+        {
+            Level13();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level13()
+        {
+            Level14();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level14()
+        {
+            Level15();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level15()
+        {
+            Level16();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level16()
+        {
+            Level17();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level17()
+        {
+            Level18();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level18()
+        {
+            Level19();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level19()
+        {
+            Level20();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Level20()
+        {
+            counter++;
+            // will *always* happen, but makes it a fair comparision
+            // so both benchmarks pay the cost 'counter++' and 'counter % chanceOfAsteroidHit'
+            if (counter % chanceOfAsteroidHit >= 0)
+                throw new InvalidOperationException("Deep Stack Trace");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel1()
+        {
+            RareLevel2();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel2()
+        {
+            RareLevel3();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel3()
+        {
+            RareLevel4();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel4()
+        {
+            RareLevel5();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel5()
+        {
+            RareLevel6();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel6()
+        {
+            RareLevel7();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel7()
+        {
+            RareLevel8();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel8()
+        {
+            RareLevel9();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel9()
+        {
+            RareLevel10();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel10()
+        {
+            RareLevel11();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel11()
+        {
+            RareLevel12();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel12()
+        {
+            RareLevel13();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel13()
+        {
+            RareLevel14();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel14()
+        {
+            RareLevel15();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel15()
+        {
+            RareLevel16();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel16()
+        {
+            RareLevel17();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel17()
+        {
+            RareLevel18();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel18()
+        {
+            RareLevel19();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel19()
+        {
+            RareLevel20();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RareLevel20()
+        {
+            counter++;
+            // will *rarely* happen (1 in 2700)
+            if (counter % chanceOfAsteroidHit == 1)
+                throw new InvalidOperationException("Deep Stack Trace - Rarely triggered");
+        }
+    }
+}
